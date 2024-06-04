0d8d196bf8e582641e3c02d2dc1d0265967f8f28
Jakub Stříbrný <35335130+kubajznik@users.noreply.github.com>
2024-06-04 09:53:10 +0200

    do not use contact point use for Ruppiner

--- fhir-models/fhir/contactPoint.go
+++ fhir-models/fhir/contactPoint.go
@@ -23,7 +23,7 @@ type ContactPoint struct {
 	Extension []Extension         `bson:"extension,omitempty" json:"extension,omitempty"`
 	System    *ContactPointSystem `bson:"system,omitempty" json:"system,omitempty"`
 	Value     *string             `bson:"value,omitempty" json:"value,omitempty"`
-	Use       *ContactPointUse    `bson:"use,omitempty" json:"use,omitempty"`
+	Use       *string    		  `bson:"use,omitempty" json:"use,omitempty"`
 	Rank      *int                `bson:"rank,omitempty" json:"rank,omitempty"`
 	Period    *Period             `bson:"period,omitempty" json:"period,omitempty"`
 }
