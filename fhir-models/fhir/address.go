3f46ec1c2242956b64a60b8c4f73819d503b99ab
bilalgulzar <bilal.gulzar@recaresolutions.com>
2026-01-16 23:23:27 +0100

    DEV-18474 Fix LineExtension for recare address

--- fhir-models/fhir/address.go
+++ fhir-models/fhir/address.go
@@ -33,18 +33,22 @@ type Address struct {
 	Period     *Period      `bson:"period,omitempty" json:"period,omitempty"`
 }
 
+type LineExtensionItem struct {
+	Extension []RecareExtension `bson:"extension,omitempty" json:"extension,omitempty"`
+}
+
 type RecareAddress struct {
-	Id            *string           `bson:"id,omitempty" json:"id,omitempty"`
-	LineExtension []RecareExtension `bson:"_line,omitempty" json:"_line,omitempty"`
-	Use           *AddressUse       `bson:"use,omitempty" json:"use,omitempty"`
-	Type          *AddressType      `bson:"type,omitempty" json:"type,omitempty"`
-	Text          *string           `bson:"text,omitempty" json:"text,omitempty"`
-	HouseNumber   *EncryptedField   `bson:"houseNumber,omitempty" json:"houseNumber,omitempty"`
-	Line          []string          `bson:"line,omitempty" json:"line,omitempty"`
-	City          *string           `bson:"city,omitempty" json:"city,omitempty"`
-	District      *string           `bson:"district,omitempty" json:"district,omitempty"`
-	State         *string           `bson:"state,omitempty" json:"state,omitempty"`
-	PostalCode    *string           `bson:"postalCode,omitempty" json:"postalCode,omitempty"`
-	Country       *string           `bson:"country,omitempty" json:"country,omitempty"`
-	Period        *Period           `bson:"period,omitempty" json:"period,omitempty"`
+	Id            *string             `bson:"id,omitempty" json:"id,omitempty"`
+	LineExtension []LineExtensionItem `bson:"_line,omitempty" json:"_line,omitempty"`
+	Use           *AddressUse         `bson:"use,omitempty" json:"use,omitempty"`
+	Type          *AddressType        `bson:"type,omitempty" json:"type,omitempty"`
+	Text          *string             `bson:"text,omitempty" json:"text,omitempty"`
+	HouseNumber   *EncryptedField     `bson:"houseNumber,omitempty" json:"houseNumber,omitempty"`
+	Line          []string            `bson:"line,omitempty" json:"line,omitempty"`
+	City          *string             `bson:"city,omitempty" json:"city,omitempty"`
+	District      *string             `bson:"district,omitempty" json:"district,omitempty"`
+	State         *string             `bson:"state,omitempty" json:"state,omitempty"`
+	PostalCode    *string             `bson:"postalCode,omitempty" json:"postalCode,omitempty"`
+	Country       *string             `bson:"country,omitempty" json:"country,omitempty"`
+	Period        *Period             `bson:"period,omitempty" json:"period,omitempty"`
 }
